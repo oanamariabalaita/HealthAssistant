369fd035402d8cd3b02b2f6287d7a25dc4d67af1
Maria Balaita <maria.balaita@zentity.com>
2020-04-30 00:00:05 +0200

    UI custom bottom navigation view implemented

---
+++ app/src/main/java/com/example/healthassistant/design/CurvedBottomNavigationView.kt
@@ -0,0 +1,122 @@
+package com.example.healthassistant.design
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.graphics.Point
+import android.util.AttributeSet
+import androidx.core.content.ContextCompat
+import com.example.healthassistant.R
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx
+
+class CurvedBottomNavigationView : BottomNavigationViewEx {
+    /**
+     * the CURVE_CIRCLE_RADIUS represent the radius of the fab button
+     */
+    companion object {
+        const val CURVE_CIRCLE_RADIUS = 108
+    }
+
+    private var mPath = Path()
+    private var mPaint = Paint()
+
+    // the coordinates of the first curve
+    private val mFirstCurveStartPoint = Point()
+    private val mFirstCurveEndPoint = Point()
+    private val mFirstCurveControlPoint1 = Point()
+    private val mFirstCurveControlPoint2 = Point()
+
+    // the coordinates of the second curve
+    private var mSecondCurveStartPoint = Point()
+    private val mSecondCurveEndPoint = Point()
+    private val mSecondCurveControlPoint1 = Point()
+    private val mSecondCurveControlPoint2 = Point()
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init()
+    }
+
+    private fun init() {
+
+        mPaint.style = Paint.Style.FILL_AND_STROKE
+        mPaint.color = ContextCompat.getColor(context, R.color.colorPrimary)
+        setBackgroundColor(ContextCompat.getColor(context, R.color.transparent))
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        // the coordinates (x,y) of the start point before curve
+        mFirstCurveStartPoint.set(width / 2 - CURVE_CIRCLE_RADIUS * 2 - CURVE_CIRCLE_RADIUS / 8, 0)
+        // the coordinates (x,y) of the end point after curve
+        mFirstCurveEndPoint.set(width / 2, CURVE_CIRCLE_RADIUS + CURVE_CIRCLE_RADIUS / 8)
+        // same thing for the second curve
+        mSecondCurveStartPoint = mFirstCurveEndPoint
+        mSecondCurveEndPoint.set(width / 2 + CURVE_CIRCLE_RADIUS * 2 + CURVE_CIRCLE_RADIUS / 8, 0)
+
+        // the coordinates (x,y)  of the 1st control point on a cubic curve
+        mFirstCurveControlPoint1
+            .set(
+                mFirstCurveStartPoint.x + CURVE_CIRCLE_RADIUS + CURVE_CIRCLE_RADIUS / 32,
+                mFirstCurveStartPoint.y
+            )
+        // the coordinates (x,y)  of the 2nd control point on a cubic curve
+        mFirstCurveControlPoint2
+            .set(
+                mFirstCurveEndPoint.x - CURVE_CIRCLE_RADIUS * 2 + CURVE_CIRCLE_RADIUS,
+                mFirstCurveEndPoint.y
+            )
+
+        mSecondCurveControlPoint1
+            .set(
+                mSecondCurveStartPoint.x + CURVE_CIRCLE_RADIUS * 2 - CURVE_CIRCLE_RADIUS,
+                mSecondCurveStartPoint.y
+            )
+        mSecondCurveControlPoint2
+            .set(
+                mSecondCurveEndPoint.x - (CURVE_CIRCLE_RADIUS + CURVE_CIRCLE_RADIUS / 32),
+                mSecondCurveEndPoint.y
+            )
+
+        mPath.apply {
+            reset()
+            moveTo(0f, 0f)
+            lineTo(mFirstCurveStartPoint.x.toFloat(), mFirstCurveStartPoint.y.toFloat())
+
+            cubicTo(
+                mFirstCurveControlPoint1.x.toFloat(), mFirstCurveControlPoint1.y.toFloat(),
+                mFirstCurveControlPoint2.x.toFloat(), mFirstCurveControlPoint2.y.toFloat(),
+                mFirstCurveEndPoint.x.toFloat(), mFirstCurveEndPoint.y.toFloat()
+            )
+
+            cubicTo(
+                mSecondCurveControlPoint1.x.toFloat(), mSecondCurveControlPoint1.y.toFloat(),
+                mSecondCurveControlPoint2.x.toFloat(), mSecondCurveControlPoint2.y.toFloat(),
+                mSecondCurveEndPoint.x.toFloat(), mSecondCurveEndPoint.y.toFloat()
+            )
+
+            lineTo(width.toFloat(), 0f)
+            lineTo(width.toFloat(), height.toFloat())
+            lineTo(0f, height.toFloat())
+            close()
+        }
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawPath(mPath, mPaint)
+    }
+}
