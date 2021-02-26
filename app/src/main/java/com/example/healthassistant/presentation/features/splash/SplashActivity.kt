25b55941ae91efe21490720ebb87e62222b25389
Maria Balaita <maria.balaita@zentity.com>
2021-02-26 23:52:09 +0100

    Simple splash screen implemented

---
+++ app/src/main/java/com/example/healthassistant/presentation/features/splash/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.example.healthassistant.presentation.features.splash
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.healthassistant.presentation.MainActivity
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasAndroidInjector
+import javax.inject.Inject
+
+class SplashActivity : AppCompatActivity(), HasAndroidInjector {
+
+    @Inject
+    lateinit var androidInjector: DispatchingAndroidInjector<Any>
+
+    override fun androidInjector(): AndroidInjector<Any> = androidInjector
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
