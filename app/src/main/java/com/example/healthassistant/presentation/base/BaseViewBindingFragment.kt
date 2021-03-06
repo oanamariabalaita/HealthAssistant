fd89561191e8fa1dccd71821e1fd1cf4a811d1ce
Maria Balaita <maria.balaita@zentity.com>
2021-03-06 23:53:20 +0100

    Unused classes deleted

--- app/src/main/java/com/example/healthassistant/presentation/base/BaseViewBindingFragment.kt
+++ app/src/main/java/com/example/healthassistant/presentation/base/BaseViewBindingFragment.kt
@@ -4,27 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModelProvider
 import androidx.viewbinding.ViewBinding
-import com.example.healthassistant.presentation.utils.navigation.INavigationResult
-import com.example.healthassistant.presentation.utils.navigation.NavigationResult
-import com.example.healthassistant.presentation.utils.navigation.NavigationResultViewModel
 import javax.inject.Inject
 
-abstract class BaseViewBindingFragment<TViewBinding : ViewBinding> : BaseFragment(),
-    INavigationResult {
+abstract class BaseViewBindingFragment<TViewBinding : ViewBinding> : BaseFragment() {
 
     private var _viewBinding: TViewBinding? = null
     private val viewBinding get() = _viewBinding
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
-
-    private val navigationResultViewModel: NavigationResultViewModel by viewModels {
-        viewModelFactory
-    }
 
     open fun canNavigateUp() = true
 
@@ -39,18 +29,6 @@ abstract class BaseViewBindingFragment<TViewBinding : ViewBinding> : BaseFragmen
     ): View? = createBinding(inflater, container).also {
         _viewBinding = it
     }.root
-
-    override fun observeNavigationResult(
-        tag: String,
-        lifecycleOwner: LifecycleOwner,
-        callback: (navigationResult: NavigationResult) -> Unit
-    ) {
-        navigationResultViewModel.observeNavigationResult(tag, lifecycleOwner, callback)
-    }
-
-    override fun setNavigationResult(tag: String, navigationResult: NavigationResult) {
-        navigationResultViewModel.produceNavigationResult(tag, navigationResult)
-    }
 
     override fun onDestroyView() {
         super.onDestroyView()
